4ff0e668f529b2ada6ac641d1a6725ceb0934feb
lamartire <lamartire@gmail.com>
2019-07-20 16:04:07 +0300

    feat(sharec): add silent mode for install command

--- packages/sharec/README.md
+++ packages/sharec/README.md
@@ -17,8 +17,22 @@ Merge feature now available for `*.json` and `package.json` based configs. `*.js
 
 Other files will be merged by `common` strategy (shallow merging).
 
+## Commands
+
+### `install`
+
+Installs all configuration to the target project.
+
+| Option   | Default | Description                                          |
+| -------- | ------- | ---------------------------------------------------- |
+| `silent` | `false` | Configuration will be installed without any messages |
+
+### `remove`
+
+Removes installed configuration from target project. Modified fields will be saved.
+
 ## Examples
 
 - [examples.md](https://github.com/lamartire/sharec/blob/master/packages/sharec/docs/examples.md) – example notes about configuration preset creation, publishing and installing
-- [sharec-demo-config](https://www.npmjs.com/package/sharec-demo-config) – official configuration preset example
-- [essential-javascript-config](https://github.com/lamartire/essential-javascript-config) – my personal javascript-configuration preset
+- [sharec-demo-config](https://github.com/lamartire/sharec/blob/master/packages/sharec-demo-config) – official configuration preset example
+- [essential-javascript-config](https://github.com/lamartire/sharec/blob/master/packages/sharec-essential-config) – my personal javascript-configuration preset
--- packages/sharec/src/commands/install.js
+++ packages/sharec/src/commands/install.js
@@ -1,22 +1,29 @@
-const ora = require('ora')
+const createSpinner = require('../messages/spinner')
 const installTask = require('../tasks/install')
 const { collectConfigVersion } = require('../core/configs/collect')
+const { extractMetaData } = require('../core/package/extract')
 const installedMessage = require('../messages/installed')
 
-async function install({ configsPath, targetPath, options }) {
-  const spinner = ora({
-    text: 'applying configuration... 🚀',
-    spinner: 'line',
-    prefixText: 'sharec:',
-    interval: 50,
-  }).start()
+async function install({ configsPath, targetPath, options = {} }) {
   const configsVersion = await collectConfigVersion(configsPath)
+  const meta = await extractMetaData(targetPath)
+  const isSilentMode =
+    options.silent || (meta && meta.version === configsVersion)
+  const spinner = createSpinner({
+    text: 'applying configuration... 🚀',
+    silent: isSilentMode,
+  })
+
+  spinner.start()
 
   try {
     await installTask({ configsPath, targetPath, configsVersion, options })
 
     spinner.succeed('configuration applyed, have a nice time! 🌈')
-    installedMessage()
+
+    if (isSilentMode) {
+      installedMessage()
+    }
   } catch (err) {
     const { message } = err
 
@@ -26,7 +33,7 @@ async function install({ configsPath, targetPath, options }) {
       spinner.fail('configs directory was not found! ⛔️')
     } else {
       spinner.fail('unhandeled error! 💥')
-      console.erorr(err)
+      console.error(err)
     }
   }
 }
