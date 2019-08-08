9b16f2cf98042246b7ab3abeba4943794ea8a21e
lamartire <lamartire@gmail.com>
2019-08-08 10:58:58 +0300

    feat(tester): improve tests output
    test(tester): setup jest and wallaby

--- packages/sharec-tester/src/core/files/diff.js
+++ packages/sharec-tester/src/core/files/diff.js
@@ -1,29 +1,56 @@
 const chalk = require('chalk')
 const diff = require('diff')
 
-function createFilesDiff({ fixtureFile, targetFile }) {
-  const filesDiff = diff.diffChars(targetFile.source, fixtureFile.source)
+function fitSourceLines({lines, fitCount}) {
+  if (lines.length >= fitCount) return lines
 
-  if (!filesDiff || filesDiff.length === 0) {
-    return null
+  return lines.concat(new Array(fitCount - lines.length).fill(''))
+}
+
+function fuseDiffShards(shards) {
+  let result = {
+    isChanged: false,
+    value: ''
   }
 
-  const outputDiff = filesDiff.reduce((acc, part) => {
-    if (part.added) {
-      return acc + chalk.bgGreen(chalk.black(part.value))
-    } else if (part.removed) {
-      return acc + chalk.bgRed(chalk.black(part.value))
+  shards.forEach(shard => {
+    if (shard.added || shard.removed) {
+      result.isChanged = true
     }
 
-    return acc + part.value
-  }, '')
+    if (shard.added) {
+      result.value += chalk`{bgGreen {black ${shard.value}}}`
+    } else if (shard.removed) {
+      result.value += chalk`{bgRed {black ${shard.value}}}`
+    } else {
+      result.value += shard.value
+    }
+  })
+
+  return result
+}
+
+function createFilesDiff({ fixtureFile, targetFile }) {
+  const fixtureLines = fixtureFile.source.split('\n')
+  const targetLines = targetFile.source.split('\n')
+  const fittedFixtureLines = fitSourceLines({ lines: fixtureLines, fitCount: targetLines.length })
+  const fittedTargetLines = fitSourceLines({ lines: targetLines, fitCount: fixtureLines.length })
+  const linesWithDiff = []
+
+  for (let i = 0; i < fittedFixtureLines.length; i++) {
+    const currentLinesDiff = diff.diffChars(fittedFixtureLines[i], fittedTargetLines[i])
+    const { value, isChanged } = fuseDiffShards(currentLinesDiff)
+
+    linesWithDiff.push(`  ${i}:  ${value}`)
+  }
 
   return {
     fullPath: targetFile.fullPath,
-    output: outputDiff,
+    output: linesWithDiff.join('\n'),
   }
 }
 
 module.exports = {
+  fitSourceLines,
   createFilesDiff,
 }
