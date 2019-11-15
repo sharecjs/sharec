100e2d8a919d41dfdde2df6933cb101e8d82707f
lamartire <lamartire@gmail.com>
2019-11-15 21:44:36 +0300

    test(sharec): add update test with .travis.yml
    relates to issues #75

--- packages/sharec/src/test/base/update.test.js
+++ packages/sharec/src/test/base/update.test.js
@@ -5,6 +5,7 @@ const sharec = require('../../')
 describe('sharec > update', () => {
   const packageJsonUpdateFxt = fixtures('package/json/13-base-update', 'json')
   const gitignoreUpdateFxt = fixtures('gitignore/lines/02-base-update')
+  const travisUpdateFxt = fixtures('travis/yaml/00-base-update')
   const targetProcess = {
     argv: [null, null, 'install'],
     env: {
@@ -18,16 +19,19 @@ describe('sharec > update', () => {
   })
 
   it('should correctly update configs', async () => {
-    expect.assertions(4)
+    expect.assertions(5)
 
     const dir = {
       '/target/package.json': JSON.stringify(packageJsonUpdateFxt.current),
       '/target/.gitignore': gitignoreUpdateFxt.current,
+      '/target/.travis.yml': travisUpdateFxt.current,
       '/target/node_modules/.cache/sharec/awesome-config/1.0.0/package.json': JSON.stringify(
         packageJsonUpdateFxt.cached,
       ),
       '/target/node_modules/.cache/sharec/awesome-config/1.0.0/gitignore':
         gitignoreUpdateFxt.cached,
+      '/target/node_modules/.cache/sharec/awesome-config/1.0.0/.travis.yml':
+        travisUpdateFxt.cached,
       '/configuration-package/package.json': JSON.stringify({
         name: 'awesome-config',
         version: '2.0.0',
@@ -36,6 +40,7 @@ describe('sharec > update', () => {
         packageJsonUpdateFxt.upcoming,
       ),
       '/configuration-package/configs/gitignore': gitignoreUpdateFxt.upcoming,
+      '/configuration-package/configs/.travis.yml': travisUpdateFxt.upcoming,
     }
     vol.fromJSON(dir, '/')
 
@@ -61,5 +66,11 @@ describe('sharec > update', () => {
         'utf8',
       ),
     ).toEqual(gitignoreUpdateFxt.upcoming)
+    expect(
+      vol.readFileSync(
+        '/target/node_modules/.cache/sharec/awesome-config/2.0.0/.travis.yml',
+        'utf8',
+      ),
+    ).toEqual(travisUpdateFxt.result)
   })
 })
