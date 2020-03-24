55a58cb102c3709fa8773ab21016dbcd8efe57cb
lamartire <lamartire@gmail.com>
2020-03-24 18:17:56 +0300

    test(sharec): remove remove test for old yaspeller

--- packages/sharec/src/core/strategies/instances/test/yaspeller/base.test.js
+++ packages/sharec/src/core/strategies/instances/test/yaspeller/base.test.js
@@ -13,15 +13,6 @@ describe('strategy > yaspeller', () => {
     ).toMatchObject(yaspellerBaseFxt.result)
   })
 
-  it('should remove applyed JSON config', () => {
-    expect(
-      yaspellerStrategy.unapplyJSON({
-        current: yaspellerBaseFxt.result,
-        upcoming: yaspellerBaseFxt.upcoming,
-      }),
-    ).toMatchObject(yaspellerBaseFxt.restored)
-  })
-
   it('should fully unapply JSON config and return empty object', () => {
     expect(
       yaspellerStrategy.unapplyJSON({
