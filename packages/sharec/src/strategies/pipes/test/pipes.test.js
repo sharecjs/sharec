0d802ac3fe093b8bbe59681ccc032b94eae4acc5
lamartire <lamartire@gmail.com>
2020-03-31 13:23:31 +0300

    feat(sharec): add fallback pipes for json and yaml files

--- packages/sharec/src/strategies/pipes/test/pipes.test.js
+++ packages/sharec/src/strategies/pipes/test/pipes.test.js
@@ -1,11 +1,32 @@
-const { getConfigPipe } = require('../index')
+const { getConfigPipe, getFallbackConfigPipe } = require('../index')
 
-describe('strategies > pipes > getConfigPipe', () => {
-  it('should return pipe by given config path', () => {
-    expect(getConfigPipe('/configs/.eslintrc')).not.toBeNull()
+describe('strategies > pipes', () => {
+  describe('getFallbackConfigPipe', () => {
+    it('should return fallback pipe for json files', () => {
+      expect(getFallbackConfigPipe('/configs/foo.json')).not.toBeNull()
+    })
+
+    it('should return fallback pipe for yaml files', () => {
+      expect(getFallbackConfigPipe('/configs/foo.yaml')).not.toBeNull()
+    })
+
+    it('should return null if file has not fallback pipe', () => {
+      expect(getFallbackConfigPipe('/configs/.simpsonsrc')).toBeNull()
+    })
   })
 
-  it('should return null if pipe is not exist for given config', () => {
-    expect(getConfigPipe('/configs/.simpsonsrc')).toBeNull()
+  describe('getConfigPipe', () => {
+    it('should return pipe by given config path', () => {
+      expect(getConfigPipe('/configs/.eslintrc')).not.toBeNull()
+    })
+
+    it('should return fallback pipes for json and yaml files', () => {
+      expect(getConfigPipe('/configs/foo.json')).not.toBeNull()
+      expect(getConfigPipe('/configs/foo.yaml')).not.toBeNull()
+    })
+
+    it('should return null if pipe is not exist for given config', () => {
+      expect(getConfigPipe('/configs/.simpsonsrc')).toBeNull()
+    })
   })
 })
