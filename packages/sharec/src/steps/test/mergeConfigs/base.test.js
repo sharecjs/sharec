3baae8131ab042e698567a677f8a3466a7abc781
lamartire <lamartire@gmail.com>
2020-10-28 21:08:17 +0300

    feat(sharec): ability to overwrite specific configs

--- packages/sharec/src/steps/test/mergeConfigs/base.test.js
+++ packages/sharec/src/steps/test/mergeConfigs/base.test.js
@@ -1,10 +1,11 @@
 const { vol } = require('memfs')
-const { createFakeSpinner, createFakePrompt } = require('testUtils')
+const { fixtures, createFakeSpinner, createFakePrompt } = require('testUtils')
 const mergeConfigs = require('../../mergeConfigs')
 
 describe('steps > mergeConfigs > base', () => {
   let spinner
   let prompt
+  const defaultFxt = fixtures('default/json/00-base')
 
   beforeEach(() => {
     spinner = createFakeSpinner()
@@ -12,34 +13,67 @@ describe('steps > mergeConfigs > base', () => {
     vol.reset()
   })
 
-  it('should write basic configs from input to target dir', async () => {
-    const targetPackage = {
-      name: 'awesome-project',
-      version: '0.0.0',
-    }
-    const upcomingPackage = {
-      name: 'awesome-config',
-      version: '0.0.0',
-    }
-    const upcomingConfigs = {
-      '.editorconfig': 'bar',
-    }
-    const input = {
-      targetPath: '/target',
-      configs: upcomingConfigs,
-      mergedConfigs: {},
-      options: {},
-      upcomingPackage,
-    }
-    const dir = {
-      '/target/package.json': JSON.stringify(targetPackage),
-    }
-    vol.fromJSON(dir, '/configs')
+  context('without sharec config', () => {
+    it('merges configs', async () => {
+      const targetPackage = {
+        name: 'awesome-project',
+        version: '0.0.0',
+      }
+      const upcomingPackage = {
+        name: 'awesome-config',
+        version: '0.0.0',
+      }
+      const upcomingConfigs = {
+        '.editorconfig': 'bar',
+      }
+      const input = {
+        targetPath: '/target',
+        configs: upcomingConfigs,
+        mergedConfigs: {},
+        options: {},
+        upcomingPackage,
+      }
+      const dir = {
+        '/target/package.json': JSON.stringify(targetPackage),
+      }
+      vol.fromJSON(dir, '/configs')
 
-    const output = await mergeConfigs({ spinner, prompt })(input)
+      const output = await mergeConfigs({ spinner, prompt })(input)
 
-    expect(output.mergedConfigs).toEqual({
-      '/target/.editorconfig': upcomingConfigs['.editorconfig'],
+      expect(output.mergedConfigs).toEqual({
+        '/target/.editorconfig': upcomingConfigs['.editorconfig'],
+      })
+    })
+  })
+
+  context('with sharec config', () => {
+    it('overwrites forced configs', async () => {
+      const upcomingConfigs = {
+        'foo.json': defaultFxt.upcoming,
+      }
+      const input = {
+        targetPath: '/target',
+        configs: upcomingConfigs,
+        sharecConfig: {
+          configs: {
+            'foo.json': {
+              overwrite: true,
+            },
+          },
+        },
+        mergedConfigs: {},
+        options: {},
+      }
+      const dir = {
+        '/target/foo.json': defaultFxt.current,
+      }
+      vol.fromJSON(dir, '/configs')
+
+      const output = await mergeConfigs({ spinner, prompt })(input)
+
+      expect(output.mergedConfigs).toEqual({
+        '/target/foo.json': defaultFxt.upcoming,
+      })
     })
   })
 })
