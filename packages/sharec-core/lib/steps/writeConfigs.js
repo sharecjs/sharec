075f22b6319aa886d7d7a86a9a1590cf261943fc
Konstantin Epishev <konstantin@epishev.me>
2022-03-27 23:02:11 +0200

    fix: dirty hack to force eof when write configs

--- packages/sharec-core/lib/steps/writeConfigs.js
+++ packages/sharec-core/lib/steps/writeConfigs.js
@@ -1,4 +1,5 @@
 // @ts-check
+const { EOL } = require('os')
 const { writeFile } = require('sharec-utils').std
 const { dirname } = require('sharec-utils').path
 const { safeMakeDir } = require('sharec-utils').fs
@@ -6,6 +7,8 @@ const { safeMakeDir } = require('sharec-utils').fs
 /**
  * @typedef {import('../').FlowContext} FlowContext
  */
+
+const eolRegexp = new RegExp(`${EOL}$`)
 
 /**
  * @param {FlowContext} context
@@ -15,8 +18,16 @@ const writeConfigs = async (context) => {
   const { mergedConfigs } = context
 
   for (const config in mergedConfigs) {
+    const configContent = mergedConfigs[config]
+
     await safeMakeDir(dirname(config))
-    await writeFile(config, mergedConfigs[config])
+
+    // FIXME: temp solution, then need to create additional middleware for this
+    if (eolRegexp.test(configContent)) {
+      await writeFile(config, configContent)
+    } else {
+      await writeFile(config, `${configContent}${EOL}`)
+    }
   }
 
   return context
