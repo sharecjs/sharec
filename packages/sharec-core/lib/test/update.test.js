3d89ada342c1d2e2a6633f9d8ebf2072d4a38456
Konstantin Epishev <konstantin@epishev.me>
2022-03-31 23:24:10 +0200

    test: update specs

--- packages/sharec-core/lib/test/update.test.js
+++ packages/sharec-core/lib/test/update.test.js
@@ -1,151 +1,156 @@
-const path = require('path')
 const { fixtures } = require('testUtils')
 const { vol } = require('memfs')
 const { sharec } = require('../')
 
-describe('sharec > update > linear', () => {
-  const packageFxt = fixtures('package/json/02-update')
-  const babelFxt = fixtures('babel/json/05-update')
-  const eslintFxt = fixtures('eslint/json/02-update')
-  const gitignoreFxt = fixtures('gitignore/lines/02-update')
-  const npmignoreFxt = fixtures('npmignore/lines/02-update')
-  const yaspellerFxt = fixtures('yaspeller/json/01-update')
-  const defaultJsonFxt = fixtures('default/json/00-base')
-  const defaultYamlFxt = fixtures('default/yaml/00-base')
+describe.only('sharec > update', () => {
+  describe('linear', () => {
+    const packageFxt = fixtures('package/json/02-update')
+    const babelFxt = fixtures('babel/json/05-update')
+    const eslintFxt = fixtures('eslint/json/02-update')
+    const gitignoreFxt = fixtures('gitignore/lines/02-update')
+    const npmignoreFxt = fixtures('npmignore/lines/02-update')
+    const yaspellerFxt = fixtures('yaspeller/json/01-update')
+    const defaultJsonFxt = fixtures('default/json/00-base')
+    const defaultYamlFxt = fixtures('default/yaml/00-base')
+    const context = {
+      targetPath: '/target',
+      cache: {},
+      configs: [],
+      options: {
+        cache: true,
+      },
+    }
 
-  const input = {
-    targetPath: '/target',
-    configPath: '/configuration-package',
-  }
+    beforeEach(() => {
+      jest.clearAllMocks()
+      vol.reset()
+    })
 
-  beforeEach(() => {
-    jest.clearAllMocks()
-    vol.reset()
+    it('updates configs', async () => {
+      const dir = {
+        // Upcoming
+        '/target/node_modules/awesome-config/configs/folder/foo.json': defaultJsonFxt.upcoming,
+        '/target/node_modules/awesome-config/configs/folder/foo.yaml': defaultYamlFxt.upcoming,
+        '/target/node_modules/awesome-config/configs/.babelrc': babelFxt.upcoming,
+        '/target/node_modules/awesome-config/configs/.eslintrc': eslintFxt.upcoming,
+        '/target/node_modules/awesome-config/configs/.gitignore': gitignoreFxt.upcoming,
+        '/target/node_modules/awesome-config/configs/.npmignore': npmignoreFxt.upcoming,
+        '/target/node_modules/awesome-config/configs/.yaspellerrc': yaspellerFxt.upcoming,
+        '/target/node_modules/awesome-config/configs/package.json': packageFxt.upcoming,
+        '/target/node_modules/awesome-config/package.json': JSON.stringify({
+          name: 'awesome-config',
+          version: '2.0.0',
+        }),
+        // Cached
+        '/target/node_modules/.cache/sharec/folder/foo.json': defaultJsonFxt.cached,
+        '/target/node_modules/.cache/sharec/folder/foo.yaml': defaultYamlFxt.cached,
+        '/target/node_modules/.cache/sharec/.babelrc': babelFxt.cached,
+        '/target/node_modules/.cache/sharec/.eslintrc': eslintFxt.cached,
+        '/target/node_modules/.cache/sharec/.gitignore': gitignoreFxt.cached,
+        '/target/node_modules/.cache/sharec/.npmignore': npmignoreFxt.cached,
+        '/target/node_modules/.cache/sharec/.yaspellerrc': yaspellerFxt.cached,
+        '/target/node_modules/.cache/sharec/package.json': packageFxt.cached,
+        // Current
+        '/target/folder/foo.json': defaultJsonFxt.current,
+        '/target/folder/foo.yaml': defaultYamlFxt.current,
+        '/target/.babelrc': babelFxt.current,
+        '/target/.eslintrc': eslintFxt.current,
+        '/target/.gitignore': gitignoreFxt.current,
+        '/target/.npmignore': npmignoreFxt.current,
+        '/target/.yaspellerrc': yaspellerFxt.current,
+        '/target/package.json': packageFxt.current,
+      }
+
+      vol.fromJSON(dir, '/')
+
+      await sharec(context)
+
+      expect(vol.readFileSync('/target/folder/foo.json', 'utf8')).toMatchFxt(defaultJsonFxt.result)
+      expect(vol.readFileSync('/target/folder/foo.yaml', 'utf8')).toMatchFxt(defaultYamlFxt.result)
+      expect(vol.readFileSync('/target/.babelrc', 'utf8')).toMatchFxt(babelFxt.result)
+      expect(vol.readFileSync('/target/.eslintrc', 'utf8')).toMatchFxt(eslintFxt.result)
+      expect(vol.readFileSync('/target/.gitignore', 'utf8')).toMatchFxt(gitignoreFxt.result)
+      expect(vol.readFileSync('/target/.npmignore', 'utf8')).toMatchFxt(npmignoreFxt.result)
+      expect(vol.readFileSync('/target/.yaspellerrc', 'utf8')).toMatchFxt(yaspellerFxt.result)
+      expect(vol.readFileSync('/target/package.json', 'utf8')).toMatchFxt(packageFxt.result)
+    })
   })
 
-  it.only('should update configs in the target project', async () => {
-    const cacheBasePath = '/target/node_modules/.cache/sharec/awesome-config/1.0.0'
-    const upcomingPackage = {
-      name: 'awesome-config',
-      version: '2.0.0',
+  describe('with removed fields', () => {
+    const packageFxt = fixtures('package/json/05-removed')
+    const context = {
+      targetPath: '/target',
+      cache: {},
+      configs: [],
+      options: {
+        cache: true,
+      },
     }
-    const dir = {
-      // Upcoming
-      '/configuration-package/configs/.babelrc': babelFxt.upcoming,
-      '/configuration-package/configs/.eslintrc': eslintFxt.upcoming,
-      '/configuration-package/configs/.gitignore': gitignoreFxt.upcoming,
-      '/configuration-package/configs/.npmignore': npmignoreFxt.upcoming,
-      '/configuration-package/configs/.yaspellerrc': yaspellerFxt.upcoming,
-      '/configuration-package/configs/foo.json': defaultJsonFxt.upcoming,
-      '/configuration-package/configs/foo.yaml': defaultYamlFxt.upcoming,
-      '/configuration-package/configs/package.json': packageFxt.upcoming,
-      '/configuration-package/package.json': JSON.stringify(upcomingPackage),
-      // Cached
-      [path.join(cacheBasePath, '.babelrc')]: babelFxt.cached,
-      [path.join(cacheBasePath, '.eslintrc')]: eslintFxt.cached,
-      [path.join(cacheBasePath, '.gitignore')]: gitignoreFxt.cached,
-      [path.join(cacheBasePath, '.npmignore')]: npmignoreFxt.cached,
-      [path.join(cacheBasePath, '.yaspellerrc')]: yaspellerFxt.cached,
-      [path.join(cacheBasePath, 'foo.json')]: defaultJsonFxt.cached,
-      [path.join(cacheBasePath, 'foo.yaml')]: defaultYamlFxt.cached,
-      [path.join(cacheBasePath, 'package.json')]: packageFxt.cached,
-      // Current
-      '/target/.babelrc': babelFxt.current,
-      '/target/.eslintrc': eslintFxt.current,
-      '/target/.gitignore': gitignoreFxt.current,
-      '/target/.npmignore': npmignoreFxt.current,
-      '/target/.yaspellerrc': yaspellerFxt.current,
-      '/target/foo.json': defaultJsonFxt.current,
-      '/target/foo.yaml': defaultYamlFxt.current,
-      '/target/package.json': packageFxt.current,
-    }
-    vol.fromJSON(dir, '/')
 
-    await sharec(input)
+    beforeEach(() => {
+      jest.clearAllMocks()
+      vol.reset()
+    })
 
-    expect(vol.readFileSync('/target/.babelrc', 'utf8')).toWraplessEqual(babelFxt.result)
-    expect(vol.readFileSync('/target/.eslintrc', 'utf8')).toWraplessEqual(eslintFxt.result)
-    expect(vol.readFileSync('/target/.gitignore', 'utf8')).toWraplessEqual(gitignoreFxt.result)
-    expect(vol.readFileSync('/target/.npmignore', 'utf8')).toWraplessEqual(npmignoreFxt.result)
-    expect(vol.readFileSync('/target/.yaspellerrc', 'utf8')).toWraplessEqual(yaspellerFxt.result)
-    expect(vol.readFileSync('/target/foo.json', 'utf8')).toWraplessEqual(defaultJsonFxt.result)
-    expect(vol.readFileSync('/target/foo.yaml', 'utf8')).toWraplessEqual(defaultYamlFxt.result)
-    expect(vol.readFileSync('/target/package.json', 'utf8')).toWraplessEqual(packageFxt.result)
-  })
-})
+    it('updates configs', async () => {
+      const dir = {
+        // Upcoming
+        '/target/node_modules/awesome-config/configs/package.json': packageFxt.upcoming,
+        '/target/node_modules/awesome-config/package.json': JSON.stringify({
+          name: 'awesome-config',
+          version: '2.0.0',
+        }),
+        // Cached
+        '/target/node_modules/.cache/sharec/package.json': packageFxt.cached,
+        // Current
+        '/target/package.json': packageFxt.current,
+      }
+      vol.fromJSON(dir, '/')
 
-describe('sharec > update > with removed fields', () => {
-  const packageFxt = fixtures('package/json/05-removed')
+      await sharec(context)
 
-  const input = {
-    targetPath: '/target',
-    configPath: '/configuration-package',
-  }
-
-  beforeEach(() => {
-    jest.clearAllMocks()
-    vol.reset()
+      expect(vol.readFileSync('/target/package.json', 'utf8')).toMatchFxt(packageFxt.result)
+    })
   })
 
-  it('should update configs in the target project', async () => {
-    const cacheBasePath = '/target/node_modules/.cache/sharec/awesome-config/1.0.0'
-    const upcomingPackage = {
-      name: 'awesome-config',
-      version: '2.0.0',
+  describe('with removed configs', () => {
+    const eslintFxt = fixtures('eslint/json/02-update')
+    const packageFxt = fixtures('package/json/05-removed')
+    const context = {
+      targetPath: '/target',
+      cache: {},
+      configs: [],
+      options: {
+        cache: true,
+      },
     }
-    const dir = {
-      // Upcoming
-      '/configuration-package/configs/package.json': packageFxt.upcoming,
-      '/configuration-package/package.json': JSON.stringify(upcomingPackage),
-      // Cached
-      [path.join(cacheBasePath, 'package.json')]: packageFxt.cached,
-      // Current
-      '/target/package.json': packageFxt.current,
-    }
-    vol.fromJSON(dir, '/')
 
-    await sharec(input)
+    beforeEach(() => {
+      jest.clearAllMocks()
+      vol.reset()
+    })
 
-    expect(vol.readFileSync('/target/package.json', 'utf8')).toWraplessEqual(packageFxt.result)
-  })
-})
+    it('updates configs', async () => {
+      const dir = {
+        // Upcoming
+        '/target/node_modules/awesome-config/configs/package.json': packageFxt.upcoming,
+        '/target/node_modules/awesome-config/configs/.eslintrc': eslintFxt.upcoming,
+        '/target/node_modules/awesome-config/package.json': JSON.stringify({
+          name: 'awesome-config',
+          version: '2.0.0',
+        }),
+        // Cached
+        '/target/node_modules/.cache/sharec/package.json': packageFxt.cached,
+        '/target/node_modules/.cache/sharec/.eslintrc': eslintFxt.cached,
+        // Current
+        '/target/package.json': packageFxt.current,
+      }
+      vol.fromJSON(dir, '/')
 
-describe('sharec > update > with removed configs', () => {
-  const eslintFxt = fixtures('eslint/json/02-update')
-  const packageFxt = fixtures('package/json/05-removed')
+      await sharec(context)
 
-  const input = {
-    targetPath: '/target',
-    configPath: '/configuration-package',
-  }
-
-  beforeEach(() => {
-    jest.clearAllMocks()
-    vol.reset()
-  })
-
-  it('should update configs in the target project', async () => {
-    const cacheBasePath = '/target/node_modules/.cache/sharec/awesome-config/1.0.0'
-    const upcomingPackage = {
-      name: 'awesome-config',
-      version: '2.0.0',
-    }
-    const dir = {
-      // Upcoming
-      '/configuration-package/configs/package.json': packageFxt.upcoming,
-      '/configuration-package/configs/.eslintrc': eslintFxt.upcoming,
-      '/configuration-package/package.json': JSON.stringify(upcomingPackage),
-      // Cached
-      [path.join(cacheBasePath, 'package.json')]: packageFxt.cached,
-      [path.join(cacheBasePath, '.eslintrc')]: eslintFxt.cached,
-      // Current
-      '/target/package.json': packageFxt.current,
-    }
-    vol.fromJSON(dir, '/')
-
-    await sharec(input)
-
-    expect(vol.readdirSync('/target')).not.toContain('.eslintrc')
-    expect(vol.readFileSync('/target/package.json', 'utf8')).toWraplessEqual(packageFxt.result)
+      expect(vol.readdirSync('/target')).not.toContain('.eslintrc')
+      expect(vol.readFileSync('/target/package.json', 'utf8')).toMatchFxt(packageFxt.result)
+    })
   })
 })
