86dafea6a96e15391ef5ceba5d4bc3438b03b7db
Konstantin Epishev <konstantin@epishev.me>
2022-04-04 23:06:47 +0200

    feat: update read runtime config step

--- packages/sharec-core/lib/steps/readRuntimeConfig.js
+++ packages/sharec-core/lib/steps/readRuntimeConfig.js
@@ -4,25 +4,33 @@ const { join } = require('sharec-utils').path
 
 /**
  * @typedef {import('../').FlowContext} FlowContext
+ * @typedef {import('../').Semaphore} Semaphore
  */
 
 /**
  * TODO: don't know how to cover the function with unit-tests
  * `require` can't work inside `memfs`
  * @param {FlowContext} context
+ * @param {Semaphore} semaphore
  * @returns {Promise<FlowContext>}
  */
-const readRuntimeConfig = async (context) => {
+const readRuntimeConfig = async (context, semaphore) => {
   const { targetPath } = context
   const runtimeConfigPath = join(targetPath, '.sharecrc.js')
 
   try {
+    semaphore.start('Loading runtime configuration')
+
     await stat(runtimeConfigPath)
   } catch (err) {
+    semaphore.error('No runtime configuration found')
+
     return context
   }
 
   context.runtimeConfig = require(runtimeConfigPath)
+
+  semaphore.success('Runtime configuration has been loaded')
 
   return context
 }
