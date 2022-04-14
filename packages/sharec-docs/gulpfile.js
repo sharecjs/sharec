32a135abd7a08666d7cdff333ab0e9a7ba267f67
Konstantin Epishev <konstantin@epishev.me>
2022-04-14 23:44:01 +0200

    chore: reload docs site on any change

--- packages/sharec-docs/gulpfile.js
+++ packages/sharec-docs/gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp')
-const { serve } = require('./task/serve')
+const { serve, reload } = require('./task/serve')
 const { css } = require('./task/css')
 const { svg } = require('./task/svg')
 const { fonts, cname, images } = require('./task/static')
@@ -14,16 +14,19 @@ gulp.task('serve', serve)
 
 // Watch tasks
 gulp.task('watch:css', () =>
-  gulp.watch(['src/**/*.css', 'src/**/*.liquid'], css),
+  gulp.watch(['src/**/*.css', 'src/**/*.liquid'], gulp.series(css, reload)),
 )
 gulp.task('watch:svg', () =>
-  gulp.watch('src/**/*.svg', svg),
+  gulp.watch('src/**/*.svg', gulp.series(svg, reload)),
 )
 gulp.task('watch:fonts', () =>
-  gulp.watch('src/assets/fonts/**/*', fonts),
+  gulp.watch('src/assets/fonts/**/*', gulp.series(fonts, reload)),
 )
 gulp.task('watch:images', () =>
-  gulp.watch('src/assets/images/**/*', images),
+  gulp.watch('src/assets/images/**/*', gulp.series(images, reload)),
+)
+gulp.task('watch:html', () =>
+  gulp.watch('dist/**/*.html', reload),
 )
 
 // Core tasks
@@ -34,6 +37,7 @@ gulp.task(
     'watch:svg',
     'watch:fonts',
     'watch:images',
+    'watch:html',
   ),
 )
 gulp.task(
