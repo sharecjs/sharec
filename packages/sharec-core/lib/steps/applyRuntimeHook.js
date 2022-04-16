c67eaeb6fa00d40e4bc2d07c0a2c6b1465fcc047
Konstantin Epishev <konstantin@epishev.me>
2022-04-17 00:17:50 +0200

    fix: pass semaphore to the hooks

--- packages/sharec-core/lib/steps/applyRuntimeHook.js
+++ packages/sharec-core/lib/steps/applyRuntimeHook.js
@@ -2,6 +2,7 @@
 
 /**
  * @typedef {import('../').FlowContext} FlowContext
+ * @typedef {import('../').Semaphore} Semaphore
  */
 
 /**
@@ -13,14 +14,15 @@ const readRuntimeConfig = (hook) =>
    * TODO: don't know how to cover the function with unit-tests
    * `require` can't work inside `memfs`
    * @param {FlowContext} context
+   * @param {Semaphore} semaphore
    * @returns {Promise<FlowContext>}
    */
-  async (context) => {
+  async (context, semaphore) => {
     const { runtimeConfig = {} } = context
 
     if (!runtimeConfig[hook]) return context
 
-    return runtimeConfig[hook](context)
+    return runtimeConfig[hook](context, semaphore)
   }
 
 module.exports = readRuntimeConfig
